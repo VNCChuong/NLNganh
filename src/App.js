dd9776850271dfd5465666e61b86d05a0f131e60
duy <duyb2014646@student.ctu.edu.vn>
2024-03-15 22:33:40 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { routes } from './routes/index'
+import DefaultComponent from './components/DefaultComponent/DefaultComponent'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+
 
 function App() {
+
+  // useEffect(() => {
+  //   fetchApi()
+  // }, [])
+  // const REACT_API_URI_BACKEND = 'http://localhost:3001/api' || process.env.REACT_API_URI_BACKEND
+  const fetchApi = async () => {
+    const res = await axios.get(`http://localhost:3001/api/product/get-all`)
+    return res.data
+  }
+  const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi })
+  console.log('query = ', query)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          {routes.map((route) => {
+
+            const Page = route.page
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment
+            return (
+              <Route key={route.path} path={route.path} element={
+                <Layout>
+                  <Page />
+                </Layout>
+              } />
+            )
+          })}
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
